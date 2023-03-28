6737c7dd3e269447d4a0ea86b0125360d57ba19e
ymolyavkin <mat2017u@yandex.ru>
2023-03-28 08:37:05 +0500

    feat : I checked the post-request handling using the method from the Practicum - it works, the body of the request is received.

--- src/kanban/tasksAPI/HttpTaskServer.java
+++ src/kanban/tasksAPI/HttpTaskServer.java
@@ -2,6 +2,7 @@ package kanban.tasksAPI;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import kanban.core.HttpTaskManager;
@@ -35,8 +36,70 @@ public class HttpTaskServer implements HttpHandler {
         this.httpTaskManager = httpTaskManager;
     }
 
+
     @Override
-    public void handle(HttpExchange exchange) throws IOException {
+    public void handle(HttpExchange httpExchange) throws IOException {
+        String response;
+
+        // извлеките метод из запроса
+        String method = httpExchange.getRequestMethod();
+
+        switch (method) {
+            // сформируйте ответ в случае, если был вызван POST-метод
+            case "POST":
+                System.out.println("Получен POST");
+                // извлеките тело запроса
+                InputStream inputStream = httpExchange.getRequestBody();
+
+
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String body = bufferedReader.readLine();
+                System.out.println("body" + body);
+                String body2 = new String(inputStream.readAllBytes(), DEFAULT_CHARSET);
+                System.out.println(body2);
+                //String body = ... bufferedReader.readLine();
+
+                // извлеките path из запроса
+                String path = httpExchange.getRequestURI().getPath();
+                String[] splitStrings = path.split("/");
+                String profession = splitStrings[2];
+                String name = splitStrings[3];
+                // String path = ...
+                // а из path — профессию и имя
+                //  String profession = ...
+                //    String name = ...
+
+                // извлеките заголовок
+                Headers requestHeaders = httpExchange.getRequestHeaders();
+                // List<String> wishGoodDay = ...
+                List<String> wishGoodDay = requestHeaders.get("X-Wish-Good-Day");
+                if ((wishGoodDay != null) && (wishGoodDay.contains("true"))) {
+                    response = body + profession + ", " + name + "!" + "Хорошего дня!";
+                } else {
+                    // {приветствие}, {профессия} {имя}!».
+                    response = "Привет, " + profession + " " + name + "!";
+                }
+
+                // соберите ответ
+                //  response = ...
+                // не забудьте про ответ для остальных методов
+                break;
+            case "GET":
+                //response = "Вы использовали метод GET!";
+                response = "Здравствуйте!";
+                break;
+            default:
+                response = "Некорректный метод!";
+        }
+
+
+        httpExchange.sendResponseHeaders(200, 0);
+        try (OutputStream os = httpExchange.getResponseBody()) {
+            os.write(response.getBytes());
+        }
+    }
+    //@Override
+    public void handleOld(HttpExchange exchange) throws IOException {
 
 
         String method = exchange.getRequestMethod();
